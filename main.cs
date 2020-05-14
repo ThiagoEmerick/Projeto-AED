d1595c7fc735d00072b42b7668819eb33323704c
thallesZombie <thalles027@ucl.br>
2020-05-14 22:49:24 +0000

    basico

--- main.cs
+++ main.cs
@@ -1,7 +1,12 @@
 using System;
-
+using System.IO;
+using System.Text; 
 class MainClass {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+
+  public static void Main (string[] args) { 
+      Console.WriteLine("## 1 - Pessoa Fisica##"); 
+      Console.WriteLine("## 2 - pessoa Juridica##");
+      Console.WriteLine("## 3 - compra Produtos ##");
+      Console.WriteLine("## 0 - Finalizar Programa ##");  
+   }
   }
-}
